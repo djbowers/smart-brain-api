5673a2fa9714535769c0217f74c5f50e259c4910
DJ Bowers <dj@dj-bowers.com>
2019-12-07 16:54:25 -0800

    Add additional logic to config for PORT

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-module.exports = {
-  PORT: process.env.PORT,
-  CLARIFAI_API_KEY: process.env.CLARIFAI_API_KEY,
+const CLARIFAI_API_KEY = process.env.CLARIFAI_API_KEY;
+const PORT = process.env.PORT;
+
+if (PORT == null || PORT == "") {
+  PORT = 8000;
 }
+
+module.exports = { PORT, CLARIFAI_API_KEY }
