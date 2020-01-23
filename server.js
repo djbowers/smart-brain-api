45b93b050707e7ce024d3f2e55c70e76f2122967
DJ Bowers <dj@dj-bowers.com>
2020-01-22 16:08:24 -0800

    Reformat server.js

--- server.js
+++ server.js
@@ -13,30 +13,34 @@ const image = require('./controllers/image');
 const { PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: DATABASE_URL,
-        ssl: true,
-    }
-})
+  client: 'pg',
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: true
+  }
+});
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('server is live') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', register.handleRegister(db, bcrypt))
-app.get('/profile/:id', profile.handleProfileGet(db))
-app.put('/image', image.handleImage(db))
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.get('/', (req, res) => {
+  res.send('server is live');
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt));
+app.get('/profile/:id', profile.handleProfileGet(db));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 let port = PORT;
 if (port == null || port == '') {
-    port = 3001;
+  port = 3001;
 }
 
 app.listen(port, () => {
-    console.log(`app is running on port ${port}`)
-})
+  console.log(`app is running on port ${port}`);
+});
